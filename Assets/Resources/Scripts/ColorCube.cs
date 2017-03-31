1d2ac2ef8213f96050ab4238e975aa1b2ec30fbc
lunaticAnn <lunatic_ann@hotmail.com>
2017-03-31 09:29:58 -0400

    App ID

---
+++ Assets/Resources/Scripts/ColorCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCube : MonoBehaviour {
+	//vertice number in 3 dimensions
+	public int x,y,z;
+
+	ParticleSystem _par;
+
+	private void Start()
+	{
+
+	}
+
+	
+}
